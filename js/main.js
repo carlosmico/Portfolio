480125516a301867d0e41920afc7d46c79a49660
Carlos Micó <carlosmico15@gmail.com>
2019-07-09 11:50:02 +0200

    Projects border removed

--- js/main.js
+++ js/main.js
@@ -9,14 +9,17 @@ const projectsHover = () => {
     const project1 = document.querySelectorAll('.project')[0];
     const project2 = document.querySelectorAll('.project')[1];
     const project3 = document.querySelectorAll('.project')[2];
+    const project4 = document.querySelectorAll('.project')[3];
 
     const title1 = document.querySelector('.title1');
     const title2 = document.querySelector('.title2');
     const title3 = document.querySelector('.title3');
+    const title4 = document.querySelector('.title4');
 
     title1.classList.add('titleHide');
     title2.classList.add('titleHide')
     title3.classList.add('titleHide')
+    title4.classList.add('titleHide')
 
     project1.addEventListener("mouseover", event => {
         if(title1.classList.contains('titleHide')){
@@ -57,6 +60,20 @@ const projectsHover = () => {
         if(title3.classList.contains('titleShow')){
             title3.classList.remove('titleShow')
             title3.classList.add('titleHide')
+        }
+    });
+
+    project4.addEventListener("mouseover", event => {
+        if(title4.classList.contains('titleHide')){
+            title4.classList.remove('titleHide')
+            title4.classList.add('titleShow')
+        }
+    });
+
+    project4.addEventListener("mouseout", event => {
+        if(title4.classList.contains('titleShow')){
+            title4.classList.remove('titleShow')
+            title4.classList.add('titleHide')
         }
     });
 }
